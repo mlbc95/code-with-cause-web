46f6f5071ebb6d05a1707dea814d24825d2e6bd9
pandoramic <cassie.j.short@gmail.com>
2018-02-10 09:38:20 -0600

    Added material design. Added necessary components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "./model";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Code With A Cause';
+  user: User;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatInputModule} from "@angular/material";
+import { LoginComponent } from './login/login.component';
+import { ManagementComponent } from './management/management.component';
+import { EntryComponent } from './entry/entry.component';
+import { ReviewComponent } from './review/review.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    ManagementComponent,
+    EntryComponent,
+    ReviewComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
