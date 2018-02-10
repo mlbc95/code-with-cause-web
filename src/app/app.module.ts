3e5cabc1e42cb93004f90160e56f560f9e8bbfa6
Kyle Hopfer <Kyle.Hopfer@gmail.com>
2018-02-10 11:12:35 -0600

    Fixed merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,17 +15,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AuthGuard} from './guards/auth.guard';
 import {AdminGuard} from './guards/admin.guard';
 import {AuthenticationService} from './services/authentication.service';
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatInputModule} from "@angular/material";
-import { LoginComponent } from './login/login.component';
-import { ManagementComponent } from './management/management.component';
-import { EntryComponent } from './entry/entry.component';
-import { ReviewComponent } from './review/review.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 
 @NgModule({
