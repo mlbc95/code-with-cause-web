483b537505c6d2fa71a0f3117172099762ec5abd
Chau <ctch5@mail.umsl.edu>
2018-02-11 11:35:30 -0600

    Stop pushing too much Justin

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -12,11 +12,14 @@ import {IHarvestVm} from '../swagger-api/model/iHarvestVm';
 import {IEntryVm} from '../swagger-api/model/iEntryVm';
 import {EntryService} from '../swagger-api/api/entry.service';
 import {IHarvestParams} from '../swagger-api/model/iHarvestParams';
-import * as _ from 'lodash';
 import {Message} from 'primeng/api';
 import {Router} from '@angular/router';
-import {Configuration, ConfigurationParameters} from "../swagger-api/configuration";
-import {IUserVm, SystemService} from '../swagger-api';
+import {INewEntryParams, IUserVm, SystemService} from '../swagger-api';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Configuration, ConfigurationParameters} from '../swagger-api/configuration';
 
 @Component({
   selector: 'app-entry',
@@ -31,35 +34,38 @@ export class EntryComponent implements OnInit, OnDestroy {
   msgs: Message[] = [];
 
   // dropdown lists
-  farms: any[] = [];
-  organizations: any[] = [];
-  crops: any[] = [];
+  farms: IFarmVm[] = [];
+  organizations: IOrganizationVm[];
+  crops: ICropVm[];
   variety: any[] = [];
-  users: any[];
+  harvesters: IHarvesterVm[];
 
 
   selectedFarm: IFarmVm;
   harvest: IHarvestVm;
   currentEntry: IEntryVm;
   cropsList: ICropVm[];
+  cropTest: ICropVm;
+  users: IUserVm[];
 
   harvester: string;
   cropSleceted: string;
-  pounds: number;
-  priceTotal: number;
+  pounds = 0;
+  priceTotal = 0;
   farm: string;
   recipent: string;
   selectedVar: string;
-  selectedUser: string;
+  selectedHarvester: string;
   selectedOrg: string;
   comment: string;
 
-  doneLoading: boolean = false;
-  loading: boolean;
+  doneLoading = false;
 
   varieties: string[];
 
   entryIdArray: any[] = [];
+
+  form: FormGroup;
 
   constructor(private entryService: EntryService,
               private farmService: FarmService,
@@ -67,9 +73,10 @@ export class EntryComponent implements OnInit, OnDestroy {
               private harvesterService: HarvesterService,
               private organizationService: OrganizationService,
               private harvestService: HarvestService,
+              private router: Router,
               private systemService: SystemService,
-              private router: Router) {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+              private fb: FormBuilder) {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
     this.token = currentUser.token;
     let config: ConfigurationParameters = {
       apiKeys: {
@@ -84,123 +91,137 @@ export class EntryComponent implements OnInit, OnDestroy {
     harvestService.configuration = new Configuration(config);
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.today = new Date().toLocaleDateString();
     this.harvestStarted = false;
-    this.loading = true;
-    let storedHarvestID = JSON.parse(localStorage.getItem('harvest_id'));
+    this.editMode = false;
+    this.initForm();
+    Observable
+      .combineLatest(
+        this.farmService.getAll(),
+        this.cropService.getAll()
+      )
+      .subscribe((data: [IFarmVm[], ICropVm[]]) => {
+        const [farms, crops] = data;
+        this.farms = farms;
+        this.crops = crops;
+      });
+    // const storedHarvestID = JSON.parse(localStorage.getItem('harvest_id'));
     // if (storedHarvestID) {
     //   this.harvestService.getHarvestById(storedHarvestID).subscribe((harvest) => {
     //     this.harvest = harvest;
     //   });
-    //}
+    // }
+  }
 
-    this.farmService.getAll().subscribe(
-      (farms: Array<IFarmVm>): void => {
-        this.farms = farms;
-        this.loading = false;
-      },
-      (error) => {
-        console.error(error);
-        this.loading = false;
-      }
-    );
-
-    this.cropService.getAll().subscribe(
-      (crops: Array<ICropVm>): void => {
-        this.cropsList = crops;
-        crops.forEach(c => {
-          this.crops.push({label: c.name, value: c._id})
-        })
-
-        console.log(this.crops);
-        this.doneLoading = true;
-      },
-      (error) => {
-        console.log("error")
-        console.log(error);
-      }
-    );
-
-    this.today = new Date().toLocaleDateString();
-    this.editMode = false;
+  initForm() {
+    this.form = this.fb.group({
+      crop: ['', Validators.required],
+      harvester: [''],
+      farm: [''],
+      variety: [''],
+      recipient: [''],
+      comment: [''],
+      pounds: [0],
+      priceTotal: [0]
+    });
   }
 
   ngOnDestroy(): void {
-    this.entryService.configuration.apiKeys["Authorization"] = null;
-    this.farmService.configuration.apiKeys["Authorization"] = null;
-    this.cropService.configuration.apiKeys["Authorization"] = null;
-    this.harvesterService.configuration.apiKeys["Authorization"] = null;
-    this.organizationService.configuration.apiKeys["Authorization"] = null;
-    this.harvestService.configuration.apiKeys["Authorization"] = null;
+    this.entryService.configuration.apiKeys['Authorization'] = null;
+    this.farmService.configuration.apiKeys['Authorization'] = null;
+    this.cropService.configuration.apiKeys['Authorization'] = null;
+    this.harvesterService.configuration.apiKeys['Authorization'] = null;
+    this.organizationService.configuration.apiKeys['Authorization'] = null;
+    this.harvestService.configuration.apiKeys['Authorization'] = null;
   }
 
   startHarvest() {
-    let newHarvest: IHarvestParams = {'farm': this.selectedFarm._id};
-    this.harvestService.registerHarvest(newHarvest).subscribe((harvest) => {
-      console.log(harvest)
-      this.harvest = harvest;
-      this.harvest.entries = [];
-      localStorage.setItem("harvest_id", JSON.stringify({
-          harvest: this.harvest._id
-        }
-      ));
-      this.harvestStarted = true;
-    });
-
-    this.systemService.getAllUsers().subscribe(
-      (users: Array<IUserVm>): void => {
-        this.users = [];
-        users.forEach((u) => {
-          this.users.push({label:u.username,value:u._id});
-        })
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-
-    this.organizationService.getAll().subscribe(
-      (organizations: Array<IOrganizationVm>): void => {
-        organizations.forEach(o => {
-          this.organizations.push({label: o.name, value: o._id})
-        })
-        console.log(organizations)
-      },
-      (error) => {
-        console.log(error);
+    const newHarvest: IHarvestParams = {'farm': this.selectedFarm._id};
+    this.harvestService.registerHarvest(newHarvest)
+      .mergeMap((harvest: IHarvestVm) => {
+        this.harvest = harvest;
+        this.harvest.entries = [];
+        localStorage.setItem('harvest_id', JSON.stringify({harvest: this.harvest._id}));
+        this.harvestStarted = true;
+        return Observable
+          .combineLatest(
+            this.harvesterService.getAll(),
+            this.organizationService.getAll(),
+            this.systemService.getAllUsers()
+          );
+      })
+      .subscribe((data: [IHarvesterVm[], IOrganizationVm[], IUserVm[]]) => {
+        const [harvesters, organizations, users] = data;
+        this.harvesters = harvesters;
+        this.users = users;
+        this.organizations = organizations;
       });
+
   }
 
   submitEntry() {
+    const newEntry: INewEntryParams = {
+      crop: this.form.get('crop').value,
+      selectedVariety: this.form.get('variety').value,
+      recipient: this.form.get('recipient').value,
+      pounds: this.form.get('pounds').value,
+      comments: this.form.get('comment').value,
+      harvester: this.form.get('harvester').value,
+      priceTotal: this.form.get('priceTotal').value
+    };
 
-    let newEntry = {'crop': this.cropSleceted, 'pounds': this.pounds, 'priceTotal': this.priceTotal, 'harvester': this.selectedUser, 'comments': this.comment, 'recipient': this.selectedOrg};
-    this.entryService.registerEntry(newEntry).subscribe(
-      (entry: IEntryVm): void => {
-        console.log(entry);
+    this.entryService.registerEntry(newEntry)
+      .subscribe((entry: IEntryVm) => {
+        console.log('New Entry', entry);
         this.msgs = [];
         this.msgs.push({severity: 'success', summary: 'Success', detail: 'Entry Saved! You\'re saving Trees'});
-        this.entryIdArray.push(entry._id)
+        this.entryIdArray.push(entry._id);
         localStorage.setItem('entry_id', JSON.stringify({
           entries: this.entryIdArray
         }));
-      },
-      (error) => {
+        this.form.reset();
+        this.priceTotal = 0;
+        this.pounds = 0;
+      }, (error: HttpErrorResponse) => {
         console.log(error);
       });
+    // const newEntry: INewEntryParams = {
+    //   crop: this.cropSleceted,
+    //   selectedVariety: this.selectedVar,
+    //   recipient: this.selectedOrg,
+    //   pounds: this.pounds,
+    //   comments: this.comment,
+    //   harvester: this.harvester,
+    //   priceTotal: this.priceTotal
+    // };
+    // this.entryService.registerEntry(newEntry).subscribe(
+    //   (entry: IEntryVm): void => {
+    //     console.log(entry);
+    //     this.msgs = [];
+    //     this.msgs.push({severity: 'success', summary: 'Success', detail: 'Entry Saved! Your saving Trees'});
+    //     this.entryIdArray.push(entry._id);
+    //     localStorage.setItem('entry_id', JSON.stringify({
+    //       entries: this.entryIdArray
+    //     }));
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   });
 
   }
 
   submitHarvest() {
     let harvestId = JSON.parse(localStorage.getItem('harvest_id'));
-    let entryId = JSON.parse(localStorage.getItem('entry_id'))
-    console.log(entryId.entries)
-    console.log(harvestId.harvest)
+    let entryId = JSON.parse(localStorage.getItem('entry_id'));
+    console.log(entryId.entries);
+    console.log(harvestId.harvest);
 
-    let harvestParams = {farm: this.selectedFarm._id, entries: entryId.entries, harvestId: harvestId.harvest}
-    console.log(harvestParams)
+    let harvestParams = {farm: this.selectedFarm._id, entries: entryId.entries, harvestId: harvestId.harvest};
+    console.log(harvestParams);
     this.harvestService.registerHarvest(harvestParams)
       .subscribe(data => {
-          this.router.navigate([`/review-harvest/${harvestId.harvest}`])
+          this.router.navigate([`/review-harvest/${harvestId.harvest}`]);
         },
         (error) => {
           console.log(error);
@@ -208,43 +229,88 @@ export class EntryComponent implements OnInit, OnDestroy {
     // go to review page which shows all entries, each with an edit button
   }
 
-  calcPrice() {
-    let pricePerPound;
-    let res = _.findIndex(this.cropsList, {_id: this.cropSleceted});
-    pricePerPound = this.cropsList[res].pricePerPound;
-    console.log(this.pounds, pricePerPound)
-    this.priceTotal = pricePerPound * this.pounds
+  // calcPrice() {
+  //   let pricePerPound;
+  //   let res = _.findIndex(this.cropsList, {_id: this.cropSleceted});
+  //   pricePerPound = this.cropsList[res].pricePerPound;
+  //   console.log(this.pounds, pricePerPound);
+  //   this.priceTotal = pricePerPound * this.pounds;
+  // }
+
+  // filterVar() {
+  // this.variety = [];
+  // let cropName = _.filter(this.crops, {value: this.cropSleceted});
+  // let res = _.filter(this.cropsList, {name: cropName[0].label});
+  // res.forEach(v => {
+  //   if (v.variety.length > 1) {
+  //     v.variety.forEach(vv => {
+  //       this.variety.push({label: vv, value: vv});
+  //     });
+  //   } else {
+  //     this.variety.push({label: v.variety, value: v.variety});
+
+  // calcPrice() {
+  //   let pricePerPound;
+  //   let res = _.findIndex(this.cropsList, {_id: this.cropSleceted});
+  //   pricePerPound = this.cropsList[res].pricePerPound;
+  //   console.log(this.pounds, pricePerPound);
+  //   this.priceTotal = pricePerPound * this.pounds;
+  // }
+
+  // filterVar() {
+  //   this.variety = [];
+  //   let cropName = _.filter(this.crops, {value: this.cropSleceted});
+  //   let res = _.filter(this.cropsList, {name: cropName[0].label});
+  //   res.forEach(v => {
+  //     if (v.variety.length > 1) {
+  //       v.variety.forEach(vv => {
+  //         this.variety.push({label: vv, value: vv});
+  //       });
+  //     } else {
+  //       this.variety.push({label: v.variety, value: v.variety});
+  //
+  //     }
+  //   });
+  // }
+
+  // sendHarvest(); {
+
+  // clear this harvest from local storage
+//   localStorage;
+// .
+
+//   removeItem(
+//
+//   'harvest_id';
+// );
+//   this;
+// .
+//   harvestStarted = false;
+// }
+
+// backAnEntry();
+// {
+//   console.log(this.currentEntry);
+// }
+
+// goToEntry();
+// {
+//
+// }
+
+
+  onCropChanged($event) {
+    // this.cropSleceted = $event;
+    // // console.log(this.crops);
+    // console.log($event);
+    this.cropTest = this.crops.filter(c => c._id === $event.value)[0];
+    this.varieties = this.cropTest.variety;
   }
 
-  filterVar() {
-    this.variety = [];
-    let cropName = _.filter(this.crops, {value: this.cropSleceted})
-    let res = _.filter(this.cropsList, {name: cropName[0].label});
-    res.forEach(v => {
-      if (v.variety.length > 1) {
-        v.variety.forEach(vv => {
-          this.variety.push({label: vv, value: vv})
-        })
-      } else {
-        this.variety.push({label: v.variety, value: v.variety})
-
-      }
-    })
+//
+  onPoundChanged($event) {
+    this.pounds = $event.value;
+    this.priceTotal = parseFloat((this.pounds * this.cropTest.pricePerPound).toFixed());
+    this.form.get('priceTotal').setValue(this.priceTotal);
   }
-
-  sendHarvest() {
-
-    // clear this harvest from local storage
-    localStorage.removeItem('harvest_id');
-    this.harvestStarted = false;
-  }
-
-  backAnEntry() {
-    console.log(this.currentEntry);
-  }
-
-  goToEntry() {
-
-  }
-
 }
