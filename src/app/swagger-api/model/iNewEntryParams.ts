46a6d8dec660591eb2abd1d0d12d73541455af72
Chau <ctch5@mail.umsl.edu>
2018-02-15 09:24:05 -0600

    Updated Swagger

--- src/app/swagger-api/model/iNewEntryParams.ts
+++ src/app/swagger-api/model/iNewEntryParams.ts
@@ -14,7 +14,6 @@
 export interface INewEntryParams {
     pounds: number;
     cropId?: string;
-    priceTotal?: number;
     harvesterId?: string;
     comments?: string;
     recipientId?: string;
