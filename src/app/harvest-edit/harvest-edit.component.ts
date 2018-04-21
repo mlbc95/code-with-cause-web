0e4a0eb0b489356e2ab1715a8da0088ce52972b7
scope <spencer.cope@tdktech.com>
2018-04-21 11:12:48 -0500

    get all for harvests

--- src/app/harvest-edit/harvest-edit.component.ts
+++ src/app/harvest-edit/harvest-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CropClient, CropVm} from "../app.api";
+import {HarvestClient, HarvestVm} from "../app.api";
+import {MatDialog, MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-harvest-edit',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HarvestEditComponent implements OnInit {
 
-  constructor() { }
+  token: string;
+  harvests: Array<HarvestVm> = [];
+  loading: boolean;
+
+  constructor(private harvestService: HarvestClient,
+              private matDialog: MatDialog,
+              private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.harvestService.getAll().subscribe(
+      (harvests: Array<HarvestVm>): void => {
+        this.harvests = harvests;
+        this.loading = false;
+
+        console.log(this.harvests);
+      },
+      (error: Error): void => {
+        console.error(error);
+        this.loading = false;
+        this.snackBar.open(
+          'Failed to fetch harvests',
+          'OK',
+          {
+            duration: 2000,
+            panelClass: 'snack-bar-danger'
+          }
+        );
+      }
+    );
+
   }
 
 }
