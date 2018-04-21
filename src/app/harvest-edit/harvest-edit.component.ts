b4674072603ded8ee7565b2d21ce7d187cb02689
scope <spencer.cope@tdktech.com>
2018-04-21 12:29:42 -0500

    update farm things

--- src/app/harvest-edit/harvest-edit.component.ts
+++ src/app/harvest-edit/harvest-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {CropClient, CropVm} from "../app.api";
 import {HarvestClient, HarvestVm} from "../app.api";
 import {MatDialog, MatSnackBar} from "@angular/material";
+import {EditHarvestDialogComponent} from "./edit-harvest-dialog/edit-harvest-dialog.component";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-harvest-edit',
@@ -42,8 +43,62 @@ export class HarvestEditComponent implements OnInit {
     );
   }
 
-  editHarvest(): void {
-    console.log("edit harvey");
-  }
+    editHarvest(harvest: HarvestVm): void {
+      const dialogRef = this.matDialog.open(EditHarvestDialogComponent,
+        {
+          width: '90vw',
+          data: harvest
+        }
+      );
 
+      dialogRef.afterClosed().subscribe(
+        (harvestForm: FormGroup): void => {
+          if (harvestForm) {
+            this.loading = true;
+            this.harvestService.updateFarm(harvest._id, harvestForm.value.selectedFarmId).subscribe(
+              (response: any): void => {
+                this.harvestService.getAll().subscribe(
+                  (harvests: Array<HarvestVm>): void => {
+                    this.harvests = harvests;
+                    this.loading = false;
+                    this.snackBar.open(
+                      'Harvest updated',
+                      'OK',
+                      {
+                        duration: 2000,
+                        panelClass: 'snack-bar-success'
+                      }
+                    );
+                  },
+                  (error: Error): void => {
+                    console.error(error);
+                    this.loading = false;
+                    this.snackBar.open(
+                      'Failed to fetch harvest',
+                      'OK',
+                      {
+                        duration: 2000,
+                        panelClass: 'snack-bar-danger'
+                      }
+                    );
+                  }
+                );
+              },
+              (error: Error): void => {
+                console.error(error);
+                this.loading = false;
+                this.snackBar.open(
+                  'Failed to update harvest',
+                  'OK',
+                  {
+                    duration: 2000,
+                    panelClass: 'snack-bar-danger'
+                  }
+                );
+              }
+            );
+          }
+        }
+      );
+  }
 }
