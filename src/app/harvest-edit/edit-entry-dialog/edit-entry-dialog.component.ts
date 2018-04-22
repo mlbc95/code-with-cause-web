5cae69c908a56c55e9171719b1df1f929bdea427
scope <spencer.cope@tdktech.com>
2018-04-21 22:54:31 -0500

    hmm

--- src/app/harvest-edit/edit-entry-dialog/edit-entry-dialog.component.ts
+++ src/app/harvest-edit/edit-entry-dialog/edit-entry-dialog.component.ts
@@ -1,7 +1,16 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatSnackBar} from '@angular/material';
 import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {EntryClient, EntryVm} from '../../app.api';
+import {
+  CropClient,
+  CropVm, EntryClient, EntryVm, FarmClient, FarmVm, HarvestClient, HarvesterClient, HarvesterVm, HarvestVm,
+  OrganizationClient,
+  OrganizationVm, UserClient,
+  UserVm
+} from '../../app.api';
+import {Message} from "primeng/api";
+import {Router} from "@angular/router";
+import {combineLatest} from "rxjs/observable/combineLatest";
 
 @Component({
   selector: 'app-edit-entry-dialog',
@@ -12,26 +21,92 @@ export class EditEntryDialogComponent implements OnInit {
   editEntryForm: FormGroup;
   token: string;
   harvests: Array<EntryVm> = [];
+  today: string;
+  harvestStarted: boolean;
+  editMode: boolean;
+  msgs: Message[] = [];
+
+  // dropdown lists
+  farms: FarmVm[] = [];
+  organizations: OrganizationVm[];
+  crops: CropVm[];
+  variety: any[] = [];
+  harvesters: HarvesterVm[];
+  selectedFarm: FarmVm;
+  harvest: HarvestVm;
+  cropTest: CropVm;
+  users: UserVm[];
+  harvester: string;
+  pounds = 0;
+  priceTotal = 0;
+  farm: string;
+  comment: string;
+  firstEntry = false;
+  entryCounts = 0;
   loading: boolean;
+  varieties: string[];
+  entryIdArray: any[] = [];
 
   constructor(@Inject(MAT_DIALOG_DATA) private entry: EntryVm,
               private formBuilder: FormBuilder,
               private entryService: EntryClient,
               private matDialog: MatDialog,
-              private snackBar: MatSnackBar) {
-    console.log("inthe construct");
+              private snackBar: MatSnackBar,
+              private farmService: FarmClient,
+              private cropService: CropClient,
+              private harvesterService: HarvesterClient,
+              private organizationService: OrganizationClient,
+              private harvestService: HarvestClient,
+              private router: Router,
+              private userService: UserClient,
+              private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.loading = true;
-    console.log(this.entry._id);
-    this.entryService.getSingleEntry(this.entry._id).subscribe(farms => {
-      this.editEntryForm = this.formBuilder.group({
-        crop: [this.entry.crop, Validators.required],
+    this.today = new Date().toLocaleDateString();
+    this.harvestStarted = false;
+    this.editMode = false;
+    this.initForm();
+
+    combineLatest(
+      this.farmService.getAll(),
+      this.cropService.getAll(),
+      this.userService.getAllUsers()
+    ).subscribe((data: [FarmVm[], CropVm[], UserVm[]]) => {
+      const [farms, crops, users] = data;
+      this.farms = farms;
+      this.crops = crops;
+      this.users = users;
+    });
+  }
+
+  initForm() {
+    this.entryService.getSingleEntry(this.entry._id).subscribe(entrySelected => {
+      this.editEntryForm = this.fb.group({
+        crop: [entrySelected.crop, Validators.required],
+      });
+      this.editEntryForm.get('crop').valueChanges.subscribe(() => {
+        this.editEntryForm.get('pounds').enable();
       });
       this.loading = false;
     }, error2 => {
       console.log(error2);
     });
   }
+
+
+  onCropChanged($event) {
+    this.cropTest = this.crops.filter(c => c._id === $event.value)[0];
+    this.varieties = this.cropTest.variety;
+  }
+
+  onPoundChanged($event) {
+    if ($event.value > 0 && this.cropTest) {
+      this.pounds = $event.value;
+      this.priceTotal = parseFloat((this.pounds * this.cropTest.pricePerPound).toFixed());
+      this.editEntryForm.get('priceTotal').setValue(this.priceTotal);
+    }
+  }
+
 }
